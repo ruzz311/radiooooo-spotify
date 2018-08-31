bbbb9edd8f1ae88d223932fb51aef15775577c91
Joe Longstreet <joelongstreet@gmail.com>
2018-08-31 10:28:31 -0500

    add progress messaging, dont create empty spotify playlists
    add a readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# radiooooo-spotify
+
+Creates spotify playlists based on decade and mood, songs found via [radiooooo](http://radiooooo.com/).
+
+## Running
+You'll need to create a .env file with the following properties in order to auth and use Spotify's API: SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET. You can create your own app [here](https://developer.spotify.com/web-api/).
+
+The app is currently only available via a command line tool. After creating your .env file, open a terminal, `cd` to this directory and run `npm start`;
--- index.js
+++ index.js
@@ -6,25 +6,50 @@ const spotify = require('./lib/spotify');
 const util = require('./lib/util');
 const r = require('./lib/r');
 
+const ui = new inquirer.ui.BottomBar();
+
 async function run() {
-  const { moods } = await inquirer.prompt([question.getMoods()]);
-  const { decade } = await inquirer.prompt([question.getDecades()]);
+  const { moods } = await inquirer.prompt(question.getMoods());
+  const { decade } = await inquirer.prompt(question.getDecades());
+
+  ui.log.write(`Finding countries with ${util.humanJoin(moods)} music from the ${decade}'s...`);
+
   const countries = await question.getCountries(decade, moods);
   const { country } = await inquirer.prompt(countries);
 
-  const songs = await r.getSongs(16, decade, country, moods);
-  const { title, description } = util.createPlaylistMeta(songs, decade, country);
-  const playlistId = await spotify.createPlaylist(title, description);
+  ui.log.write(`Searching radiooooo for ${util.humanJoin(moods)} songs from the ${decade}'s made in ${country}...`);
 
+  const songs = await r.getSongs(15, decade, country, moods);
+  if (!songs.length) throw new Error('Could not find songs matching your search critera.');
+
+  const artists = [...new Set(songs.map(s => s.artists))];
+  ui.log.write(`Found ${songs.length} songs, including work from ${util.humanJoin(artists)}.`);
+
+  ui.log.write('Searching spotify for tracks discovered from radiooooo...');
   const trackUris = await Promise.all(
-    songs.map(async (song) => {
-      return spotify.getTrackUri(song.artists, song.title, song.album);
-    }),
+    songs.map(
+      async song => spotify.getTrackUri(song.artists, song.title, song.album),
+    ),
   );
 
+  ui.log.write(`Found ${trackUris.length} matching tracks in spotify.`);
+  if (!trackUris.length) throw new Error('Could not find any matching tracks in radiooooo.');
+
+  const { title, description } = util.createPlaylistMeta(songs, decade, country);
+  ui.log.write(`Creating a spotify playlist named ${title}...`);
+  const playlistId = await spotify.createPlaylist(title, description);
+
+  ui.log.write(`Adding ${trackUris.length} tracks to spotify...`);
   await spotify.addTracksToPlaylist(playlistId, trackUris.filter(i => i));
+
+  ui.log.write(`Success! Check spotify for your new playlist: ${title}`);
 }
 
 (async () => {
-  await run();
+  try {
+    await run();
+  } catch (e) {
+    ui.log.write('Could not complete request :(');
+    ui.log.write(e);
+  }
 })();
